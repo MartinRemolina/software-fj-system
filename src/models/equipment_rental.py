d93da2d02a54ce2c471f9e390c4f3a1fd81e4031
tatianasarmiento2005 <tatianasarmiento.2005@gmail.com>
2026-04-28 14:39:17 -0500

    Create equipment_rental.py

---
+++ src/models/equipment_rental.py
@@ -0,0 +1,19 @@
+from src.models.service import Service 
+
+class EquipmentRental(Service):
+  def __init__(self,namr, base_price, insurance_fee=15.0):
+    # pasamos el nombre y precio base a clase padre (service) 
+    super().__init__(name,base_price)
+    # Añadimos un atributo unico para el servicio : seguro 
+    self.insurance_fee = insurance_fee
+
+  def calculate_cost(self, duration):
+    """
+    sobreescritura (poliformismo):
+    costo = (precio base * duracion) + seguro fijo 
+    """
+
+    return (self.base_price * duration) + self.insurance_fee
+
+ def description(self):
+     return f"Alquiler de Equipo: {self.name} (incluye seguro de {self.insurance_fee})"

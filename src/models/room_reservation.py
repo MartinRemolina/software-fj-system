f879b0d0506627ba910f96b385f9b1b726745bf0
Martin Remolina <martinremolina3@gmail.com>
2026-05-03 21:23:31 -0500

    Improved RoomReservation with validation and documentation

--- src/models/room_reservation.py
+++ src/models/room_reservation.py
@@ -1,10 +1,33 @@
 from src.models.service import Service
+from src.exceptions.custom_exceptions import InvalidDataError
 
 
 class RoomReservation(Service):
+    """
+    Concrete service that represents a room reservation.
+
+    This service calculates the total cost based on the number of hours
+    and validates that the input parameters are correct.
+    """
 
     def calculate_cost(self, hours=1):
+        """
+        Calculates the cost of the reservation.
+
+        :param hours: Number of hours for the reservation
+        :return: Total cost
+        :raises InvalidDataError: If hours is invalid
+        """
+
+        if hours <= 0:
+            raise InvalidDataError("Hours must be greater than 0")
+
         return self.base_price * hours
 
     def description(self):
+        """
+        Returns a description of the service.
+
+        :return: String description
+        """
         return f"Room reservation service: {self.name}"
